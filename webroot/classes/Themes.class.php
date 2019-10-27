3b44906dcfab564a313275fa7e8da0e8db475654
rafsb <r_sb@live.com>
2019-10-27 16:12:48 -0300

    cerated separeted theme's files

--- webroot/classes/Themes.class.php
+++ webroot/classes/Themes.class.php
@@ -1,46 +1,15 @@
 <?php
 class Themes extends Activity
 {
-    private static $themes = [
-        "light" => [
-            "BACKGROUND"    => "#FFFFFF"
-            , "FOREGROUND"  => "#ECF1F2"
-            , "FONT"        => "#2C3D4F"
-            , "FONTBLURED"  => "#7E8C8D"
-            , "SPAN"        => "#2980B9"
-            , "DISABLED" =>  "#BDC3C8"
-            , "DARK1"    => "rgba(0,0,0,.16)"
-            , "DARK2"    => "rgba(0,0,0,.32)"
-            , "DARK3"    => "rgba(0,0,0,.64)"
-            , "LIGHT1"   => "rgba(255,255,255,.16)"
-            , "LIGHT2"   => "rgba(255,255,255,.32)"
-            , "LIGHT3"   => "rgba(255,255,255,.64)"
-        ]
-        , "dark" => [
-            "BACKGROUND"    => "#000000"
-            , "FOREGROUND"  => "#252526"
-            , "FONT"        => "#ECF1F2"
-            , "FONTBLURED"  => "#BDC3C8"
-            , "SPAN"        => "#3398DB"
-            , "DISABLED" =>  "#95A5A6"
-            , "DARK1"    => "rgba(0,0,0,.08)"
-            , "DARK2"    => "rgba(0,0,0,.16)"
-            , "DARK3"    => "rgba(0,0,0,.32)"
-            , "LIGHT1"   => "rgba(255,255,255,.08)"
-            , "LIGHT2"   => "rgba(255,255,255,.16)"
-            , "LIGHT3"   => "rgba(255,255,255,.32)"
-        ]
-    ];
-
     public static function default(){
-        return Convert::json(self::$themes["light"]);
+        return IO::read("etc/themes.d/default.json");
     }
 
     public static function light(){
-        return Convert::json(self::$themes["light"]);
+        return IO::read("etc/themes.d/light.json");
     }
 
     public static function dark(){
-        return Convert::json(self::$themes["dark"]);
+        return IO::read("etc/themes.d/dark.json");
     }
 }
